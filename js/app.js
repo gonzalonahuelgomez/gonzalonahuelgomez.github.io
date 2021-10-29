cf6e4c09b977086f3623def3f5485c70937270e9
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2021-10-29 16:26:29 -0300

    cambios

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 setTimeout(()=> {
     document.getElementById("loader-container").style.display = "none"
     document.getElementById("home").style.display = "block"
+    document.querySelector("body").style.backgroundColor = "white"
   }, 2500)
