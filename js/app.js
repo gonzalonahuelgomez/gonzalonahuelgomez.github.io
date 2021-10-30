a8753c41414fb2cb477eb4f7481ef2bbec23c329
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2021-10-29 21:13:07 -0300

    TP0 finish

--- js/app.js
+++ js/app.js
@@ -3,3 +3,24 @@ setTimeout(()=> {
     document.getElementById("home").style.display = "block"
     document.querySelector("body").style.backgroundColor = "white"
   }, 2500)
+
+if (window.screen.width < 720) {
+    document.getElementById("quote").innerHTML = '<h1 id="quote">COMPRA <span class="text-color">ROPA</span> QUE DURE MÁS QUE LAS <span class="text-color">MODAS</span></h1>'
+}
+
+(function () {
+  
+  var forms = document.querySelectorAll('.needs-validation')
+
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
